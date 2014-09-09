f3b32616afbb8ceb78a066c807b00f2a662a4f37
Coleen Thomas <coleen.thomas@spsmail.cuny.edu>
2014-09-08 23:42:20 -0400

    answers to week 2 assignment
    ct3080a solutions for week 2 assignment

---
+++ week 2 assignment.r
@@ -0,0 +1,19 @@
+# Assign the five individuals to a vector called queue
+> queue <- c("James", "Mary", "Steve", "Alex", "Patricia")
+> queue
+#2. Update the queue for the arrival of a new patron named Harold
+> queue2 <- c(queue, "Harold")
+> queue2
+# 3. Update the queue to reflect the fact that James has finished checking out.
+> queue <- [c(2:5)]
+# 4. Update the queue to reflect the fact that Pam has talked her way in front of Steve with just one item.
+> queue <- c("Mary", "Pam", "Steve", "Alex", "Patricia", "Harold")
+> queue
+# 5. Update the queue to reflect the fact that Harold has grown impatient and left
+> queue[1:6]
+# 6. Update the queue to reflect the fact that Alex has grown impatient and left. 
+> rm("Alex")
+# 7. Identify the position of Patricia in the queue.
+> queue <<= Patricia
+# 8. Count the number of people in the queue.
+> count(queue)
