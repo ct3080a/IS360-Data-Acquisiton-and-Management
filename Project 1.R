16645cbf2e6813ba175417cba2b88ff1d9931b94
Coleen Thomas <coleen.thomas@spsmail.cuny.edu>
2014-09-16 22:15:35 -0400

    functions project

--- Project 1.R
+++ Project 1.R
@@ -1,7 +1,17 @@
 x <- c(23, 53, 2, NA, 10, 9, 1)
 x
-#median 
-factor(x)[4]
-median(x)
+# first quartile and third quartile
+nummy <- cut(x, breaks = 3, labels = c("firstq", "secondq", "thirdq"))
+data.frame(table(nummy))
+#mean
+sum((x + x)/3, na.rm = T)
+#sum of missing values
+sum(is.na(x))
+#median
+(sort(x))[2]
+# minimum
+(sort(x))[1]
 #maximum
-factor(x)[0]
+(sort(x))[3]
+# sd
+sd(x, na.rm = T)
