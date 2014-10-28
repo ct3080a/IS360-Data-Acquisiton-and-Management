3825369aa964eab4350dc06b9f48b5b3d85edca9
ct3080a <coleen.thomas@spsmail.cuny.edu>
2014-10-27 22:48:01 -0400

    week 2 second attempt

--- week 2 assignment.r
+++ week 2 assignment.r
@@ -1,19 +1,23 @@
-# Assign the five individuals to a vector called queue
-> queue <- c("James", "Mary", "Steve", "Alex", "Patricia")
-> queue
+# 1. Assign the five individuals to a vector called queue
+queue <- c("James", "Mary", "Steve", "Alex", "Patricia")
+
 #2. Update the queue for the arrival of a new patron named Harold
-> queue2 <- c(queue, "Harold")
-> queue2
+queue2 <- c(queue, "Harold")
+
 # 3. Update the queue to reflect the fact that James has finished checking out.
-> queue <- [c(2:5)]
+queue <- queue[-1]
+
 # 4. Update the queue to reflect the fact that Pam has talked her way in front of Steve with just one item.
-> queue <- c("Mary", "Pam", "Steve", "Alex", "Patricia", "Harold")
-> queue
+queue <- queue[c(1,2,5,3,4)]
+
 # 5. Update the queue to reflect the fact that Harold has grown impatient and left
-> queue[1:6]
+queue <- queue[1:5]
+
 # 6. Update the queue to reflect the fact that Alex has grown impatient and left. 
-> rm("Alex")
+queue <- queue[!(queue=="Alex")]
+
 # 7. Identify the position of Patricia in the queue.
-> queue <<= Patricia
+match("Patricia", queue)
+
 # 8. Count the number of people in the queue.
-> count(queue)
+length(queue)
