240fc00ce90d3097406dec9542c81d02f7da1b36
ct3080a <coleen.thomas@spsmail.cuny.edu>
2014-12-20 07:44:12 -0500

    extra credit
    r markdown and postgresql

---
+++ extraproject.sql
@@ -0,0 +1,27 @@
+﻿CREATE TABLE student(
+ INT AUTO_INCREMENT PRIMARY KEY NOT NULL
+sex CHAR NOT NULL,
+race CHAR NOT NULL,
+);
+
+CREATE TABLE substances(
+ INT AUTO_INCREMENT PRIMARY KEY NOT NULL
+REFERENCES student ( )
+cigarette bool NOT NULL
+alcohol bool NOT NULL
+marijuana bool NOT NULL
+Freq integer
+);
+
+
+SELECT COUNT(*) FROM student;
+SELECT AVG(Freq) FROM student;
+SELECT SUM(Freq) FROM student;
+SELECT FIRST(Freq) FROM student;
+SELECT LAST(Freq) FROM student;
+
+SELECT COUNT(*) FROM substances;
+SELECT AVG(Freq) FROM substances;
+SELECT SUM(Freq) FROM substances;
+SELECT FIRST(Freq) FROM substances;
+SELECT LAST(Freq) FROM substances;
